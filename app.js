04f60f928d3e70a9303ee12b68385afa0253e5cc
Emmanuel Ajibola <90062253+Ajiboss80@users.noreply.github.com>
2022-08-12 21:12:32 +0100

    Here's an update to my form validator project.
    It's just the sign-up page. Lol.
    Please send in your comments and reviews. Thanks

--- app.js
+++ app.js
@@ -1,107 +1,64 @@
-var firstName = document.querySelector(".first-name");
-var  firstNameError = document.querySelector(".error-1");
-var  lastName = document.querySelector(".last-name");
-var  lastNameError = document.querySelector(".error-2");
-var  email = document.querySelector(".email");
-var  emailError = document.querySelector(".error-3");
-var  password = document.querySelector(".password");
-var  passwordError = document.querySelector(".error-4");
-var  confirmPassword = document.querySelector(".confirm-password");
-var  confirmPasswordError = document.querySelector(".error-5");
-var  signUp = document.querySelector(".sign-up");
+// PICKING TAGS FROM THE HTML PAGE
+const container = document.querySelector('.container');
+const firstName = document.querySelector(".first-name");
+const  firstNameError = document.querySelector(".error-1");
+const  lastName = document.querySelector(".last-name");
+const  lastNameError = document.querySelector(".error-2");
+const  email = document.querySelector(".email");
+const  emailError = document.querySelector(".error-3");
+const  password = document.querySelector(".password");
+const  passwordError = document.querySelector(".error-4");
+const  confirmPassword = document.querySelector(".confirm-password");
+const  confirmPasswordError = document.querySelector(".error-5");
+const  signUp = document.querySelector(".sign-up");
 
-signUp.addEventListener("click", function() {
-    checkFirstName();
-    checkFirstNameLength();
-    checkLastName();
-    checkLastNameLength();
-    checkEmail();
-    checkEmailLength();
-    checkPassword();
-    checkConfirmPassword();
+// EVENT LISTENER TO CARRY OUT THE CHECKING FUNCTION ON CLICK OF THE SIGN UP BUTTON
+signUp.addEventListener("click", () => {
+    checkAll();
 });
 
-//CHECKING FOR AVAILABILITY AND LENGTH OF FIRST NAME
-function checkFirstName() {
-    if (firstName.value === "") {
-        firstName.classList.add("error");
-        firstName.classList.remove("success");
+//CHECKING FOR VALIDITY OF ALL INPUTS
+function checkAll(e) {
+    e.preventDefault();
+    if (firstName.value == "") {
+        firstName.style.border = '1px solid red';
+        lastName.style.border = 'none';
         firstNameError.innerText = "First Name is unavailabe.";
-    }
-    else {
-        firstName.classList.add("success");
-        firstNameError.innerText = "";
-    }
-}
-function checkFirstNameLength() {
-    if (firstName.value.length > 0 && firstName.value.length < 4 || firstName.value.length > 20) {
-        firstName.classList.add("error");
-        firstName.classList.remove("success");
-        firstNameError.innerText = "First Name should not be less than 4 characters or greater than 20 characters.";
-    }
-}
-
-//CHECKING FOR AVAILABILITY AND LENGTH OF LAST NAME
-function checkLastName() {
-    if (lastName.value === "") {
-        lastName.classList.add("error");
-        lastNameError.innerText = "Last Name is unavailabe.";
-    }
-    else {
-        lastName.classList.add("success");
+        lastName.style.border = 'none';
         lastNameError.innerText = "";
+        confirmPassword.style.border = 'none';
     }
-}
-function checkLastNameLength() {
-    if (lastName.value.length > 0 && lastName.value.length < 4 || firstName.value.length > 20) {
-        lastName.classList.add("error");
-        lastName.classList.remove("success");
-        lastNameError.innerText = "Last Name should not be less than 4 characters or greater than 20 characters.";
+    if (firstName.value !== '') {
+        firstName.style.border = '1px solid green';
+        firstNameError.innerText = "";
+        lastName.style.border = '1px solid red'
+        lastNameError.innerText = 'Last Name is unavailabe.';
     }
-}
-
-//CHECKING FOR AVAILABILITY AND CONTENT OF EMAIL
-function checkEmail() {
-    if (email.value == "") {
-        email.classList.add("error");
+    if (lastName.value !== "") {
+        lastName.style.border = '1px solid green';
+        lastNameError.innerText = "";
+        email.style.border = '1px solid red';
         emailError.innerText = "Email is unavailabe.";
-    } 
-    else {
-        email.classList.add("success");
-        emailError.innerText = '';
     }
-}
-function checkEmailLength() {
-    if (email.value.length > 0 && email.value.length < 2) {
-        email.classList.add("error");
-        email.classList.remove("success");
-        emailError.innerText = "Email is not valid.";
+    if (email.value !== "") {
+        email.style.border = '1px solid green';
+        emailError.innerText = "";
+        password.style.border = '1px solid red';
+        passwordError.innerText = 'Password must be at least 8 and not more than 16 characters';
     }
-}
-
-//CHECKING FOR AVAILABILITY AND CONTENT OF PASSWORD
-function checkPassword() {
-    if (password.value.length > 7 && password.value.length < 14) {
-        password.classList.add("success");
-        password.classList.remove("error");
-        passwordError.innerText = "";
+    if (password.value.length > 7 && password.value.length < 15 ) {
+        password.style.border = '1px solid green';
+        passwordError.innerText = '';
     }
-    else {
-        password.classList.add("error");
-        password.classList.remove("success");
-        passwordError.innerText = "*Password must include at least one uppercase letter, a number and a special character. \n*Password must be between 7 to 15 characters";
+    if (password.value.length >= 0 && password.value.length < 7 && password.value.length > 15) {
+        password.style.border = '1px solid red';
     }
-}
-
-//CHECKING IF PASSWORD AND CONFIRM PASSWORD ARE THE SAME
-function checkConfirmPassword() {
-    if (confirmPassword.value == "" || confirmPassword.value !== password.value) {
-        confirmPassword.classList.remove("success")
-        confirmPassword.classList.add("error");
-        confirmPasswordError.innerText = "Password is incorrect.";
+    if (confirmPassword.value !== password.value) {
+        confirmPassword.style.border = '1px solid red';
+        confirmPasswordError.innerText = 'Password is incorrect';
     }
-    else {
-        confirmPassword.classList.add("success");
-        confirmPasswordError.innerText = "";
+    if (confirmPassword.value == password.value) {
+        confirmPassword.style.border = 'none';
+        confirmPasswordError.innerText = '';
     }
 }
